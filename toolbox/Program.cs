1de99f50ea13cd030853ebe7b3a9bb716a96be14
LordImmaculate <101836390+LordImmaculate@users.noreply.github.com>
2024-10-04 19:37:44 +0200

    Added basic functionality

--- toolbox/Program.cs
+++ toolbox/Program.cs
@@ -1,9 +1,59 @@
-﻿namespace toolbox;
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace toolbox;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        if (args.Length == 0)
+        {
+            Console.WriteLine("No arguments were passed.");
+            return;
+        }
+
+        if (args[0] == "install")
+            Install(args[1]);
+    }
+
+    static void Install(string appName)
+    {
+        string appDataPath = "Not Initialized";
+
+        if (Environment.OSVersion.Platform == PlatformID.Win32NT)
+        {
+            appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            appDataPath = string.Concat(appDataPath + Path.DirectorySeparatorChar + appName);
+        }
+
+        Console.WriteLine($"Installing {appName}...");
+        if (!Directory.Exists(appDataPath))
+        {
+            Directory.CreateDirectory(appDataPath);
+        }
+
+        Console.WriteLine(appDataPath);
+        string url = "https://github.com/ravendevteam/scratchpad/releases/download/v1.1.0/scratchpad.exe";
+
+        DownloadFile(url, appDataPath + Path.DirectorySeparatorChar + appName + ".exe");
+        Console.WriteLine($"{appName} has been installed to {appDataPath}");
+    }
+
+    static void Update()
+    {
+        Console.WriteLine("Updating toolbox...");
+        //DownloadFile();
+    }
+
+    static void DownloadFile(string url, string fileName)
+    {
+        using (var client = new WebClient())
+        {
+            client.DownloadFile(url, fileName);
+        }
     }
 }
